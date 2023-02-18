1b07964ffba1af579c92b070c85bfe08eca898a1
VICTORY-PC <tuduov@gmail.com>
2023-02-18 17:48:50 +0100

    added dark mode toggler to nav bar

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -1,49 +1,97 @@
-import { React, useState } from "react";
-import { Transition } from "@headlessui/react";
-import FontAwesome from "react-fontawesome";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { React, useState, useEffect } from "react";
 import Link from "next/link";
-import { faCog, faCartShopping, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 function Nav() {
+  const [theme, setTheme] = useState("");
+
+  useEffect(() => {
+    const userTheme = localStorage.getItem("theme");
+    const SystemTheme = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    //  check set theme
+    if (userTheme === "dark" || (userTheme === null && SystemTheme)) {
+      document.documentElement.classList.add("dark");
+      setTheme("dark");
+      return;
+    } else {
+      document.documentElement.classList.remove("dark");
+      setTheme("light");
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    // 2. Create a function to toggle the theme
+    setTheme(theme === "light" ? "dark" : "light");
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      console.log(
+        "dark removed",
+        document.documentElement.classList.contains("dark")
+      );
+      return;
+    }
+
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  };
   const [open, isOpen] = useState(false);
   return (
     <div>
       <nav className=" shadow-lg fixed w-full z-10 backdrop-blur-md">
         <span className="w-full">
           <div className="flex items-center h-20 w-full">
-            <div className="flex items-center items mx-20 justify-between w-full flex-row-reverse">
+            <div className="flex items-center items mx-20 justify-between w-full flex-row">
               <span className="flex items-center justify-center flex-shrink-0">
                 <h1 className=" font-bold cursor-pointer text-xl">
-                  Gamers<span className=" text-purple-800">hub</span>
+                  Gamers<span className=" text-blue-500">hub</span>
                 </h1>
               </span>
               <span className="hidden md:block">
                 <div className=" ml-10 flex items-baseline space-x-4">
                   <a
                     href="#Home"
-                    className="cursor-pointer text-purple-800 font-semibold px-3 py-2 text-md hover:font-black"
+                    className="cursor-pointer text-blue-500 font-semibold px-3 py-2 text-md hover:font-black"
                   >
-                    Home<FontAwesomeIcon icon={faHome} className=" text-purple-800 ml-2" />
+                    Home
                   </a>
                   <a
                     href="#Home"
-                    className="cursor-pointer hover:bg-purple-800 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="cursor-pointer hover:bg-blue-500 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
-                    Account<FontAwesomeIcon icon={faUser} className=" text-purple-800 ml-2" />
+                    Account
                   </a>
                   <a
                     href="#Home"
-                    className="cursor-pointer hover:bg-purple-800 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="cursor-pointer hover:bg-blue-500 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
-                    Settings<FontAwesomeIcon icon={faCog} className=" text-purple-800 ml-2" />
+                    Settings
                   </a>
                   <a
                     href="#Home"
-                    className="cursor-pointer hover:bg-purple-800 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="cursor-pointer hover:bg-blue-500 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
-                    Cart<FontAwesomeIcon icon={faCartShopping} className=" text-purple-800 ml-2" />
+                    Cart
                   </a>
+                </div>
+              </span>
+              <span>
+                {/* darkmode toggler */}
+                <div className=" w-2 " onClick={toggleTheme}>
+                  {theme == "dark" ? (
+                    <FontAwesomeIcon
+                      icon={faSun}
+                      className=" text-[#ED8135]"
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={faMoon}
+                      className=" text-blue-600"
+                    />
+                  )}
                 </div>
               </span>
             </div>
@@ -52,7 +100,7 @@ function Nav() {
                 onClick={() => {
                   isOpen(!open);
                 }}
-                className="bg-purple-800 inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-purple-600 outline-none focus:ring-offset-2 focus:ring-offset-purple-800 focus:ring-white"
+                className="bg-blue-500 inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-purple-600 outline-none focus:ring-offset-2 focus:ring-offset-blue-500 focus:ring-white"
                 arial-aria-controls="mobile-menu"
                 arial-aria-expanded="false"
               >
@@ -99,24 +147,36 @@ function Nav() {
         {open ? (
           <div className=" md:hidden id=mobile-menu">
             <div className="bg-white px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <a className="cursor-pointer hover:bg-purple-800 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium" onClick={() => {
+              <a
+                className="cursor-pointer hover:bg-blue-500 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                onClick={() => {
                   isOpen(!open);
-                }}>
+                }}
+              >
                 Home
               </a>
-              <a className="cursor-pointer hover:bg-purple-800 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium" onClick={() => {
+              <a
+                className="cursor-pointer hover:bg-blue-500 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                onClick={() => {
                   isOpen(!open);
-                }}>
+                }}
+              >
                 Account
               </a>
-              <a className="cursor-pointer hover:bg-purple-800 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium" onClick={() => {
+              <a
+                className="cursor-pointer hover:bg-blue-500 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                onClick={() => {
                   isOpen(!open);
-                }}>
+                }}
+              >
                 Settings
               </a>
-              <a className="cursor-pointer hover:bg-purple-800 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium" onClick={() => {
+              <a
+                className="cursor-pointer hover:bg-blue-500 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                onClick={() => {
                   isOpen(!open);
-                }}>
+                }}
+              >
                 Cart
               </a>
             </div>
